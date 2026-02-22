777041d80d5e7c9d97e9a94d4b68dbbfa89faf5e
AayushJoshi8 <joshiaayushh8@gmail.com>
2026-02-22 12:36:59 +0530

    added space optimization code

--- house_robbery-1.cpp
+++ house_robbery-1.cpp
@@ -39,3 +39,33 @@ vector<int>dp(arr.size(),-1);
 cout<<solve(arr,0,dp);
     return 0;
 }
+
+//----------------space optimization TC:O(n),SC:O(1)------------------
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int>arr){
+   int n=arr.size();
+
+    if(n == 0) return 0;
+    if(n == 1) return arr[0];
+
+   int prev=arr[0];
+   int prev2=0;
+
+   for(int i=1;i<n;i++){
+    int pick=arr[i]+prev2;
+    int notPick=0+prev;
+
+    int curr=max(pick,notPick);
+    prev2=prev;
+    prev=curr;
+   }
+   return prev;
+}
+
+int main(){
+vector<int>arr={1,2,4};
+cout<<solve(arr);
+    return 0;
+}
