c63acf2f214144cabed9d4d4996eed3f5c512927
AayushJoshi8 <joshiaayushh8@gmail.com>
2026-02-22 12:03:42 +0530

    Update house_robbery-1.cpp

--- house_robbery-1.cpp
+++ house_robbery-1.cpp
@@ -1,27 +1,41 @@
-
-//memoization (top down)
+//simple recursion
 #include<bits/stdc++.h>
 using namespace std;
 
-int solve(vector<int>arr,vector<int>dp,int i){
-    if(i>=arr.size())return 0;
+int solve(vector<int>arr,int i){
+    if(i==0)return arr[i];
+    if(i<0)return 0;
 
-    if(dp[i]!=-1){
-        return dp[i];
-    }
+    int pick=arr[i]+solve(arr,i-2);
+    int notPick=0+solve(arr,i-1);
 
-    int pick=arr[i]+solve(arr,dp,i+2);
-    int notPick=solve(arr,dp,i+1);
+    return max(pick,notPick);
+}
+
+int main(){
+vector<int>arr={1,2,4};
+cout<<solve(arr,0);
+    return 0;
+}
+
+//--------------memoization top-down-----------------
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int>arr,int i,vector<int>dp){
+    if(i==0)return arr[i];
+    if(i<0)return 0;
+
+    if(dp[i]!=-1)return dp[i];
+    int pick=arr[i]+solve(arr,i-2,dp);
+    int notPick=0+solve(arr,i-1,dp);
 
     return dp[i]=max(pick,notPick);
 }
 
 int main(){
-vector<int>arr={2,1,4,9};
+vector<int>arr={1,2,4};
 vector<int>dp(arr.size(),-1);
-cout<<solve(arr,dp,0);
+cout<<solve(arr,0,dp);
     return 0;
 }
-
-
-
