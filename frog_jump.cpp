8d89e39e4a291943e4f7f9d0b82fda9c5f262c30
AayushJoshi8 <joshiaayushh8@gmail.com>
2026-03-09 18:11:40 +0530

    space optimization

--- frog_jump.cpp
+++ frog_jump.cpp
@@ -1,5 +1,5 @@
 
-//memoization (top-down)
+//-----------------------------memoization (top-down)-----------------------
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -31,5 +31,49 @@ vector<int>dp(n+1,-1);
 
 aayush a;
 cout<<a.frog(n-1,nums,dp);
+    return 0;
+
+}
+
+
+// -------------------------space optimization----------------------
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class aayush{
+public:
+
+int frog(int idx,vector<int> &nums,int n){
+    if(idx==0)return 0;
+
+    int prev=0;
+    int prev2=0;
+
+    for(int i=1;i<n;i++){
+        int fs=prev+ abs(nums[idx] - nums[idx-1]);
+        int ss=INT_MAX;
+        if(idx>1){
+        ss=prev2 + abs(nums[idx] - nums[idx-2]);
+        }
+    int curri=min(fs,ss);
+    prev2=prev;
+    prev=curri;
+    }
+
+    return prev;
+    
+        
+    
+}
+};
+
+
+int main(){
+vector<int>nums{10,20,30,10};
+int n=nums.size();
+
+aayush a;
+cout<<a.frog(n-1,nums,n);
     return 0;
 }
