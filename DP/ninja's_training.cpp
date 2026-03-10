0dca08be2f098e927956d778f89b910d9dea6e1f
AayushJoshi8 <joshiaayushh8@gmail.com>
2026-03-10 13:17:57 +0530

    Add files via upload
    ninja question

---
+++ DP/ninja's_training.cpp
@@ -0,0 +1,47 @@
+    
+    //memoization
+    #include<bits/stdc++.h>
+    using namespace std;
+
+    int solve(int day,int last,vector<vector<int>>& points,vector<vector<int>>& dp){
+        //Base Case
+        if(day==0){
+            int maxi=0;
+            for(int task=0;task<=2;task++){
+                if(task != last){
+                    maxi=max(maxi,points[0][task]);
+                }
+            }
+            return maxi;
+        }
+
+        if(dp[day][last]!=-1)return dp[day][last];
+
+        int maxi=0;
+
+        for(int task=0;task<3;task++){
+            if(task!=last){
+                int take=points[day][task] + solve(day-1,task,points,dp);
+                maxi=max(maxi,take);
+            }
+        }
+        return dp[day][last]=maxi;
+    }
+
+    int ninja(int n,vector<vector<int>>& points){
+            vector<vector<int>>dp(n,vector<int>(4,-1));
+
+        return solve(n-1,3,points,dp);
+    }
+    int main(){
+    int n=3;
+
+    vector<vector<int>> points = {
+            {10,40,70},
+            {20,50,80},
+            {30,60,90}
+        };
+
+        cout<<ninja(n,points);
+        return 0;
+    }
