31df3382f0ec9c2378ba7bf6900ff454d3aeb560
AayushJoshi8 <joshiaayushh8@gmail.com>
2026-03-12 18:10:03 +0530

    Add files via upload
    tabulation

---
+++ DP/unique_paths.cpp
@@ -0,0 +1,45 @@
+//tabulation (bottom-up)
+//SC & TC: O(n*m)
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(int m,int n,vector<vector<int>>& dp){
+
+    // express all states in for loop
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 && j==0){
+                dp[i][j]=1;
+            continue;
+            }
+        
+
+        int up=0;
+        int left=0;
+
+        if(i>0){
+            up=dp[i-1][j];
+        }
+        if(j>0){
+            left=dp[i][j-1];
+        }
+
+        dp[i][j]=up+left;
+        }
+    }
+    return dp[m-1][n-1];
+
+}
+
+int uniquePaths(int m,int n){
+    vector<vector<int>>dp(m,vector<int>(n,-1));
+    return solve(m,n,dp);
+}
+
+int main(){
+    int m = 3;
+    int n = 2;
+
+    cout<<uniquePaths(m,n);
+    return 0;
+}
